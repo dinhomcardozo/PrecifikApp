3ecaec96521ee0c01219a8bba1d77d81cd525699
dinhomcardozo <dinhomcardozo@gmail.com>
2025-10-18 14:35:52 -0300

    Dashboard's firts indicators created

--- app/controllers/clients/dashboards/overview_controller.rb
+++ app/controllers/clients/dashboards/overview_controller.rb
@@ -3,6 +3,10 @@ module Clients
     class OverviewController < Clients::AuthenticatedController
       layout "application"
       def index
+        @inputs_count = Input.count
+        @subproducts_count = Subproduct.count
+        @products_count = Product.count
+        @product_portions_count = ProductPortion.count
         @banners = SystemAdmins::Banner.for_client(current_user_client.client)
       end
     end
