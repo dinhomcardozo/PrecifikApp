70f62ab858fcee79509acf12e46ddf60f4a54c56
dinhomcardozo <dinhomcardozo@gmail.com>
2025-10-26 19:05:49 -0300

    Add client_id FK to input_types; associate input types per client

--- app/controllers/input_types_controller.rb
+++ app/controllers/input_types_controller.rb
@@ -1,28 +1,26 @@
 class InputTypesController < Clients::AuthenticatedController
   include AuthorizationForClients
   before_action :authenticate_user_client!
-  before_action :set_client_id, only: %i[create update]
   layout "application"
-  
+
   def index
-    @input_types = InputType.all
+    @input_types = InputType.where(client_id: Current.user_client.client_id)
   end
 
   def show
-    @input_type = InputType.find(params[:id])
+    @input_type = current_client_input_types.find(params[:id])
   end
 
   def new
-    @input_type = InputType.new
+    @input_type = current_client_input_types.new
   end
 
   def edit
-    @input_type = InputType.find(params[:id])
+    @input_type = current_client_input_types.find(params[:id])
   end
 
   def create
-    @input_type = InputType.new(input_type_params)
-    @input_type.client_id = Current.user_client.client_id
+    @input_type = current_client_input_types.new(input_type_params)
 
     if @input_type.save
       redirect_to input_types_path, notice: "Tipo de insumo criado com sucesso."
@@ -32,8 +30,7 @@ class InputTypesController < Clients::AuthenticatedController
   end
 
   def update
-    @input_type = InputType.find(params[:id])
-    @input_type.client_id = Current.user_client.client_id
+    @input_type = current_client_input_types.find(params[:id])
 
     if @input_type.update(input_type_params)
       redirect_to input_types_path, notice: "Tipo de insumo atualizado com sucesso."
@@ -48,13 +45,7 @@ class InputTypesController < Clients::AuthenticatedController
     params.require(:input_type).permit(:name)
   end
 
-  def set_client_id
-    if action_name == "create"
-      @input_type = InputType.new(input_type_params)
-    else
-      @input_type = InputType.find(params[:id])
-      @input_type.assign_attributes(input_type_params)
-    end
-    @input_type.client_id = Current.user_client.client_id
+  def current_client_input_types
+    InputType.where(client_id: Current.user_client.client_id)
   end
 end
